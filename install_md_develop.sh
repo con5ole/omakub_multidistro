4f9bafb8505c4c7da6022fd78f8d441f6705220c
Con5ole <ojandresen@gmail.com>
2024-06-09 19:30:33 +0200

    Add installer for develop branch

---
+++ install_md_develop.sh
@@ -0,0 +1,8 @@
+echo "Installing git..."
+sudo apt-get update > /dev/null
+sudo apt-get install -y git > /dev/null
+
+echo "Cloning stable OMAKUB..."
+git clone -b develop https://github.com/con5ole/omakub_multidistro.git ~/.local/share/omakub > /dev/null
+
+source ~/.local/share/omakub/install.sh
