e3d4ef161855e4782d61ea2e1a36f502b39cb1b2
Con5ole <ojandresen@gmail.com>
2024-06-10 17:27:32 +0200

    ulauncher.sh in debian path also handles ubuntu

--- install-Debian/ulauncher.sh
+++ install-Debian/ulauncher.sh
@@ -1,3 +1,5 @@
+distro=$(~/.local/share/omakub/distrocheck.sh)
+if [[ $distro == "Debian" ]] then
 sudo apt update && sudo apt install -y gnupg
 gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
 gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg > /dev/null
@@ -5,6 +7,15 @@ echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
           http://ppa.launchpad.net/agornostal/ulauncher/ubuntu jammy main" \
           | sudo tee /etc/apt/sources.list.d/ulauncher-jammy.list
 sudo apt update && sudo apt install -y ulauncher
+fi
+
+if [[ $distro == "Ubuntu" ]] then
+sudo add-apt-repository universe -y
+sudo add-apt-repository ppa:agornostal/ulauncher -y
+sudo apt update -y
+sudo apt install -y ulauncher
+fi
+
 
 # Start ulauncher to have it populate config before we overwrite
 mkdir -p ~/.config/autostart/
