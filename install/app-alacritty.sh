7ea938cc89d4771f2c7c0c28b569b0ede85d504b
Con5ole <ojandresen@gmail.com>
2024-06-11 15:13:14 +0200

    Alacritty older config for Debian

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,5 +1,15 @@
 sudo apt install -y alacritty
 mkdir -p ~/.config/alacritty
+distro=$(~/.local/share/omakub/distrocheck.sh)
+if [[ $distro == "Ubuntu" ]]
+then
 cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/omakub/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
 cp ~/.local/share/omakub/configs/alacritty/font.toml ~/.config/alacritty/font.toml
+fi
+
+if [[ $distro == "Debian" ]]
+then
+cp ~/.local/share/omakub/configs/alacritty.yml ~/.config/alacritty/alacritty.yml
+fi
+
