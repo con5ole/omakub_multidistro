8c1cf952f19a01047179fd1a3e8c982104496476
Con5ole <ojandresen@gmail.com>
2024-06-11 16:24:42 +0200

    neovim skip lua for debian

--- install-Debian/app-neovim.sh
+++ install-Debian/app-neovim.sh
@@ -6,7 +6,12 @@ sudo apt update
 fi
 sudo apt install -y neovim
 
-if [ ! -d "$HOME/.config/nvim" ]; then
+	if [[ $distro == "Ubuntu" ]]
+	then
+	if [ ! -d "$HOME/.config/nvim" ];
+	then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
-	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
+	distro=$(~/.local/share/omakub/distrocheck.sh)
+		cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
+	fi
 fi
