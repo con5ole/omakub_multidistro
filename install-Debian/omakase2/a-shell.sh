765fc1d1e83e60db05b1e02dd5050d4505bc9bad
Con5ole <ojandresen@gmail.com>
2024-06-15 13:44:24 +0200

    path fix darkmoss theme for bash

--- install-Debian/omakase2/a-shell.sh
+++ install-Debian/omakase2/a-shell.sh
@@ -1,4 +1,4 @@
-cp ~/.local/share/omakub/defaults/darkmoss.omp.json ~/.config/
+cp ~/.local/share/omakub/defaults/bash/darkmoss.omp.json ~/.config/
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
 cp ~/.local/share/omakub/configs/bashrc ~/.bashrc
 source ~/.local/share/omakub/defaults/bash/shell
