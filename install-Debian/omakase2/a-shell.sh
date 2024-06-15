2b67f0885f23bf9c148f07e1a7870e08522245a9
Con5ole <ojandresen@gmail.com>
2024-06-15 14:19:07 +0200

    basrc 2nd opinion fix

--- install-Debian/omakase2/a-shell.sh
+++ install-Debian/omakase2/a-shell.sh
@@ -1,6 +1,6 @@
 cp ~/.local/share/omakub/defaults/bash/darkmoss.omp.json ~/.config/
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
-cp ~/.local/share/omakub/configs/bashrc ~/.bashrc
+cp ~/.local/share/omakub/configs/bashrc2 ~/.bashrc
 source ~/.local/share/omakub/defaults/bash/shell
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
